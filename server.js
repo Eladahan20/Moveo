304b24fa77f0e6ac67a838bc5d2700393fbe43c0
Elad Dahan <eladdahan20@gmail.com>
2023-01-27 17:11:23 +0200

    Package Installed

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-const readXlsxFile = require("read-excel-file/node");
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-const fs = require('fs');
 const WebSocket = require('ws')
 
-
-API_URL = 'https://randomuser.me/api/';
 
 // Create WebSocket connection.
 const socket = new WebSocket('ws://localhost:8080');
@@ -26,38 +20,13 @@ socket.addEventListener('message', (event) => {
 // File path.
 const port = process.env.PORT || 8080;
 const app = express();
-// app.get('/', (req, res) => {
-//   request('API_URL', (error, response, body) => {
-//     if (!error && response.statusCode === 200) {
-//       res.send(body);
-//     }
-//   });
-// });
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
 
-app.get('/data', (req, res) => {
-    request(API_URL, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        res.send(body);
-      } else {
-        res.send("An error occurred while fetching data.")
-      }
-    });
-  });
 
-  app.post('/upload', upload.single('file'), (req, res) => {
-    const file = req.file;
-    const stream = fs.createReadStream(file.path);
-    stream.on('data', (chunk) => {
-      console.log(chunk)
-    });
-  stream.on('end', () => {
-      console.log('End')
-  });
-});
-
+  
 app.use("/static", express.static('./static/'));
 
 app.listen(port, () => {
